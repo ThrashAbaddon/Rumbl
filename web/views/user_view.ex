b60baf5151c47a7989512b250dd458564f492cdd
Daniel <straca369@gmail.com>
2016-11-24 13:01:22 +0100

    dodaje last_name funkciju u user_view

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -23,4 +23,26 @@ defmodule Rumbl.UserView do
         |> String.split(" ")
         |> Enum.at(0)
     end
+
+
+    @doc """
+    Parse the user's last name from the user's name field.
+
+    ## Parameters
+
+    - `user` - A User struct
+
+    ## Examples
+
+        alias Rumbl.UserView
+        alias Rumbl.User
+        user = %User{name: "Bruce Gruce"}
+        UserView.last_name(user)
+        "Gruce"
+    """
+    def last_name(%User{name: name}) do
+        name
+        |> String.split(" ")
+        |> Enum.at(1)
+    end
 end
