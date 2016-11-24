b188280e730b4524a998b6a27de79e9d33b1372e
Daniel <straca369@gmail.com>
2016-11-24 20:57:02 +0100

    editira changeset

--- web/models/user.ex
+++ web/models/user.ex
@@ -10,7 +10,15 @@ defmodule Rumbl.User do
         timestamps
     end
 
+    @doc """
+    Provjerava da li User struct sadrži `name` i `username` te da li `username` odgovara duljini od 1 do 20 znakova. Opcionalnih polja nema.
 
+    ## Parametri
+
+    - `model` - to je User struct
+    
+    - `params` - opcionalni parametri, ali nije poznato još kakvi točno.
+    """
     def changeset(model, params \\ :empty) do
         model
         |> cast(params, ~w(name username), [])
