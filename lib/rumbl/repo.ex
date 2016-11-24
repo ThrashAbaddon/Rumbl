7ed9acb9f9ca693f498bbc54a879d50b0299c518
Daniel <straca369@gmail.com>
2016-11-24 18:53:11 +0100

    prebacivanje sa spremnika u memoriji na bazu

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -2,28 +2,5 @@ defmodule Rumbl.Repo do
     @moduledoc """
     In memory repository.
     """
-  #use Ecto.Repo, otp_app: :rumbl
-
-  def all(Rumbl.User) do
-      [
-          %Rumbl.User{id: "1", name: "José Paravinjera", username: "josevalim", password: "elixir"},
-          %Rumbl.User{id: "2", name: "Bruce Gruce", username: "redrapids", password: "7langs"},
-          %Rumbl.User{id: "4", name: "Chris Boromir", username: "chrismccord", password: "phx"},
-          %Rumbl.User{id: "5", name: "Joško Boško", username: "jošković", password: "još"},
-          %Rumbl.User{id: "6", name: "Goran Popara", username: "popi", password: "345"}
-      ]
-  end
-
-  def get(module, id) do
-      Enum.find(all(module), fn(map) -> map.id == id end)
-  end
-
-    #ovo grozno izgleda
-    # todo razlomi ovo na privatne funkcije
-    def get_by(module, params) do
-        Enum.find(all(module), fn(map) ->
-            Enum.all?(params, fn({key, value}) -> Map.get(map,key) == value end)
-        end)
-    end
-
+  use Ecto.Repo, otp_app: :rumbl
 end
