de118630885fb5a535ceda02d664f8079a634ad6
Daniel <straca369@gmail.com>
2016-11-25 00:49:17 +0100

    kreira akciju create u user_controller

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -20,4 +20,14 @@ defmodule Rumbl.UserController do
         changeset = User.changeset(%User{})
         render(conn, "new.html", changeset: changeset)
     end
+
+
+    def create(conn, %{"user" => user_params}) do
+        changeset = User.changeset(%User{}, user_params)
+        {:ok, user} = Repo.insert(changeset)
+
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_parh(conn, :index))
+    end
 end
