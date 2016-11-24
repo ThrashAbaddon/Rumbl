fcb31d739b9ab8e18612737103c54cb5a45b1ab5
Daniel <straca369@gmail.com>
2016-11-24 01:22:14 +0100

    kreira show view

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.UserController do
     use Rumbl.Web, :controller
 
-    # jedina akcija je index
+    # index akcija
     def index(conn, _params) do
         # ´Repo.all´ fetches all entries from the data store matching the given query
         users = Repo.all(Rumbl.User)
@@ -9,6 +9,7 @@ defmodule Rumbl.UserController do
     end
 
     def show(conn, %{"id" => id}) do
+        # `Repo.get` fetches a single struct from the data store where the primary key matches the given id.
         user = Repo.get(Rumbl.User, id)
         render(conn, "show.html", user: user)
     end
