e4e1b3ec9b726eae8130790ad20214dc5dae8e7d
Daniel <straca369@gmail.com>
2016-11-24 12:16:39 +0100

    dodaje doc za first_name funkciju

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -2,7 +2,22 @@ defmodule Rumbl.UserView do
     use Rumbl.Web, :view
     alias Rumbl.User
 
-    #parse a user’s first name from that user’s name field
+
+    @doc """
+    Parse the user's first name from the user's name field.
+
+    ## Parameters
+
+    - `user` - A User struct
+
+    ## Examples
+
+        alias Rumbl.UserView
+        alias Rumbl.User
+        user = %User{name: "Bruce Gruce"}
+        UserView.first_name(user)
+        "Bruce"
+    """
     def first_name(%User{name: name}) do
         name
         |> String.split(" ")
