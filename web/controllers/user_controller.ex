d7fe166c30ea9c76fe27e24decb3aeb1cd10d0bc
Daniel <straca369@gmail.com>
2016-11-24 20:19:14 +0100

    kreira new akciju u UserControlleru

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.UserController do
     use Rumbl.Web, :controller
 
-    # index akcija
+
     def index(conn, _params) do
         # ´Repo.all´ fetches all entries from the data store matching the given query
         users = Repo.all(Rumbl.User)
@@ -12,5 +12,10 @@ defmodule Rumbl.UserController do
         # `Repo.get` fetches a single struct from the data store where the primary key matches the given id.
         user = Repo.get(Rumbl.User, id)
         render(conn, "show.html", user: user)
+    end
+
+    def new(conn, _params) do
+        changeset = User.changeset(%User{})
+        render(conn, "new.html", changeset: changeset)
     end
 end
