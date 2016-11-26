7eee769c5718d42f195a3fd3a4801080edccbd85
Daniel <straca369@gmail.com>
2016-11-26 16:05:36 +0100

    edit komentara za akcije

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,19 +5,41 @@ defmodule Rumbl.UserController do
 
     alias Rumbl.User
 
+    @doc """
+    Prikazuje sve korisnike koji se nalaze u spremniku podataka.
 
+    ## Parametri
+
+    - `conn` - konekcija
+    """
     def index(conn, _params) do
         users = Repo.all(Rumbl.User)
         render(conn, "index.html", users: users)
     end
 
 
+    @doc """
+    Prikazuje jednog korisnika izabirući ga po njegovom id-u iz spremnika
+    podataka.
 
+    ## Parametri
+
+    - `conn` - konekcija
+
+    - `id` - `id` pokazuje na vrijednost u danoj mapi, tj. na `id` usera
+    """
     def show(conn, %{"id" => id}) do
         user = Repo.get(Rumbl.User, id)
         render(conn, "index.html", user: user)
     end
 
+    @doc """
+    Kreira novoga korisnika u spremniku podataka.
+
+    ## Parametri
+
+    - `conn` - konekcija
+    """
     def new(conn, _params) do
         changeset = User.changeset(%User{})
         render(conn, "new.html", changeset: changeset)
@@ -31,9 +53,9 @@ defmodule Rumbl.UserController do
 
     ## Parametri
 
-    - conn - konekcija
+    - `conn` - konekcija
 
-    - user_params - `user_params` je mapa dobivena za pattern match sa "user"
+    - `user_params` - je podmapa dobivena za pattern match sa "user"
     """
     def create(conn, %{"user" => user_params}) do
         changeset = User.registration_changeset(%User{}, user_params)
@@ -59,7 +81,7 @@ defmodule Rumbl.UserController do
 
     ## Parametri
 
-    - conn - konekcija
+    - `conn` - konekcija
     """
     defp authenticate(conn, _opts) do
         if conn.assigns[:current_user] do
