82f022aaa21ec86dd58d84c8624407adfd1d72e0
Daniel <straca369@gmail.com>
2016-11-26 16:41:07 +0100

    doda /session rute

--- web/router.ex
+++ web/router.ex
@@ -18,9 +18,10 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
     
 
-    get "/greet",       GreetController,       :greet
-    get "/",            PageController,        :index
-    resources "/users", UserController, only: [:index, :show, :new, :create]
+    get       "/greet",    GreetController,          :greet
+    get       "/",         PageController,           :index
+    resources "/users",    UserController,    only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
