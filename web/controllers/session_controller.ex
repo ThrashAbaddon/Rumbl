d7840fe5e14597630b4ef18e4ab8bb1d626e26f1
Daniel <straca369@gmail.com>
2016-11-26 17:04:27 +0100

    kreira create akciju za session

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,5 +1,5 @@
 defmodule Rumbl.SessionController do
-    user Rumbl.Web, :controller
+    use Rumbl.Web, :controller
 
 
     @doc """
@@ -11,5 +11,22 @@ defmodule Rumbl.SessionController do
     """
     def new(conn, _) do
         render(conn, "new.html")
+    end
+
+    @doc """
+    Create akcija uzme `username` i `password` i ulogira na ispravno unesene podatke.
+    Ako login ne uspije onda se renderira /session/new
+    """
+    def create(conn, %{"session"=> %{"username"=> user, "password"=> pass}}) do
+        case Rumbl.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
+            {ok: conn} ->
+                conn
+                |> put_flash(:info, "Welcome back!")
+                |> redirect(to: page_path(conn, :index))
+            {error: _reason, conn} ->
+                conn
+                |> put_flash(:error, "Ivalid username/password combination.")
+                |> render("new.html")
+        end
     end
 end
