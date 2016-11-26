38c89964828355582f07b1efa2e6efc6554b5e65
Daniel <straca369@gmail.com>
2016-11-26 16:33:47 +0100

    dodaje login i implementira ga u create akciju

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -19,6 +19,8 @@ Ovaj modul definira `Rumbl.Auth` struct.
         Keyword.fetch!(opts, :repo)
     end
 
+
+
     @doc """
     Modificira konekciju tako da doda `current_user` u nju.
 
@@ -35,5 +37,23 @@ Ovaj modul definira `Rumbl.Auth` struct.
         user_id = get_session(conn, :user_id)
         user    = user_id && repo.get(Rumbl.User, user_id)
         conn = assign(conn, :current_user, user)
+    end
+
+
+
+    @doc """
+    Sprema user ID u session.
+
+    ## Parametri
+
+    - `conn` - konekcija
+
+    - `user` - trenutni korisnik
+    """
+    def login(conn, user) do
+        conn
+        |> assign(:current_user, user)
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
     end
 end
