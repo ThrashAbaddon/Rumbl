d7d527286196f2cb65361d98cdea909dcfe6385d
Daniel <straca369@gmail.com>
2016-11-27 19:56:50 +0100

    editira changeset od video

--- web/models/video.ex
+++ web/models/video.ex
@@ -10,12 +10,13 @@ defmodule Rumbl.Video do
     timestamps()
   end
 
+  @required_fields ~w(url title description)
+  @optional_fields ~w()
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
